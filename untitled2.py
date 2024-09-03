44c6a5ff761df161e674ca86f1c3d1381e6ffcc2
Omid Gervei <109640345+omid-gervei@users.noreply.github.com>
2024-09-03 18:54:28 +0330

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -4,18 +4,20 @@ Created on Sat Jul 27 20:49:15 2024
 
 @author: omidg
 """
-qq = []
-tool = 1
+
+qq = []#qq is the list that keeps the inputs with the highest number of prime divisors so far   
+tool = 1#holds the length of prime divisors of the input numbers    
 adad = 1
 for i in range(0, 10):
     
     num = int(input())
+    #maq contains all divisors of the input number
     maq = []
     for j in range(2, num):
         if num%j == 0:
             maq.append(j)
     #print(maq)
-        
+    #aval_maq contains the prime divisors of the input number    
     aval_maq = []
     for k in maq:
             for kk in range(2,k):
